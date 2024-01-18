e0052fad6570c48ab60f26dd822c6134f5a75f2c
Lucifer472 <JanvI@0427>
2024-01-18 16:37:54 +0530

    ads

--- components/ads/ads1.tsx
+++ components/ads/ads1.tsx
@@ -50,20 +50,10 @@ const PopTopAd = ({ setIsOpen }: { setIsOpen: (value: boolean) => void }) => {
   }, [pathname]);
 
   return (
-    <>
-      {loading1 ? (
-        <ClipLoader
-          color="#0e0a5f"
-          size={60}
-          cssOverride={{ borderWidth: "10px" }}
-        />
-      ) : (
-        <div
-          id="div-gpt-ad-1704975923390-0"
-          style={{ minWidth: "336px", minHeight: "280px" }}
-        ></div>
-      )}
-    </>
+    <div
+      id="div-gpt-ad-1704975923390-0"
+      style={{ minWidth: "336px", minHeight: "280px" }}
+    ></div>
   );
 };
 
