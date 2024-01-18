bceca7878e36941458cd37d1b4009cfe29b80443
Lucifer472 <JanvI@0427>
2024-01-18 17:29:21 +0530

    laoder fixes

--- components/etc/loader.tsx
+++ components/etc/loader.tsx
@@ -5,14 +5,14 @@ import { ClipLoader } from "react-spinners";
 const Loader = () => {
   const [loader, setLoader] = useState(true);
 
-  // setTimeout(() => {
-  //   setLoader(false);
-  // }, 5000);
+  setTimeout(() => {
+    setLoader(false);
+  }, 5000);
 
   return (
     <>
       {loader && (
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full flex items-center justify-center z-[99999]">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[85%] w-full h-full flex items-center justify-center z-[99999]">
           <ClipLoader
             color="#0e0a5f"
             size={60}
